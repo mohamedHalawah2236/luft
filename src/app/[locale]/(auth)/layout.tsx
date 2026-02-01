bd1f3312c98a6f79e555e78995935b89539edfa3
Mohamed Halawa <mohamedmahmoudhalawa22@gmail.com>
2026-02-01 12:06:55 +0200

    make the logo in the auth layout a link that redirects to the home page

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react';
 
 import Image from 'next/image';
+import Link from 'next/link';
 
 import AuthBanner from './_components/AuthBanner';
 
@@ -10,12 +11,17 @@ export default function layout({ children }: { children: ReactNode }) {
   return (
     <div className='flex size-full justify-between gap-8 overflow-auto bg-grayish-30 p-2 max-lg:px-10 max-md:px-5 2xl:mx-auto 2xl:max-w-[75%] 2xl:p-10'>
       <div className='flex max-h-full flex-1 flex-col gap-8 overflow-auto pe-1 lg:gap-12 lg:ps-6'>
-        <Image
-          src='/images/logo.png'
-          alt='Logo'
-          fill
-          className='!static mt-2 !h-7 !w-[4.25rem] max-lg:mx-auto lg:mt-11'
-        />
+        <Link
+          href='/'
+          className='mt-2 size-fit max-lg:mx-auto lg:mt-11'
+        >
+          <Image
+            src='/images/logo.png'
+            alt='Logo'
+            fill
+            className='!static !h-7 !w-[4.25rem]'
+          />
+        </Link>
         <QueryClientProvider>{children}</QueryClientProvider>
       </div>
       <AuthBanner />
