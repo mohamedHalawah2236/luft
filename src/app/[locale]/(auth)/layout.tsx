c36ba55a492b154a26b037ab849d60da65346e85
mohamedHalawah2236 <mohamedmahmoudhalawa22@gmail.com>
2026-02-05 16:17:56 +0200

    enhance auth layout scrolling

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -9,8 +9,8 @@ import QueryClientProvider from '@/providers/QueryClientProvider';
 
 export default function layout({ children }: { children: ReactNode }) {
   return (
-    <div className='flex size-full justify-between gap-8 overflow-auto bg-grayish-30 p-2 max-lg:items-center max-lg:px-10 max-md:px-3.5 2xl:mx-auto 2xl:max-w-[75%] 2xl:p-10'>
-      <div className='flex max-h-full flex-1 flex-col gap-8 overflow-auto pe-1 lg:gap-12 lg:ps-6'>
+    <div className='flex size-full justify-between gap-8 overflow-auto bg-grayish-30 max-lg:items-center lg:p-2 2xl:mx-auto 2xl:max-w-[75%] 2xl:p-10'>
+      <div className='flex max-h-full flex-1 flex-col gap-8 overflow-auto max-lg:p-4 max-lg:px-10 max-md:px-3.5 lg:gap-12 lg:pe-1 lg:ps-6'>
         <Link
           href='/'
           className='mt-2 size-fit max-lg:mx-auto lg:mt-11'
