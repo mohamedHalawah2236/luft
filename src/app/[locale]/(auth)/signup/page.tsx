a7f917beb24b99fe5897d1b47ad9aa9c80d7e3b0
mohamedHalawah2236 <mohamedmahmoudhalawa22@gmail.com>
2026-02-11 12:05:41 +0200

    reset testing case for signup page

--- src/app/[locale]/(auth)/signup/page.tsx
+++ src/app/[locale]/(auth)/signup/page.tsx
@@ -10,8 +10,8 @@ import VerifySignupOTPForm from './_components/VerifySignupOTPForm';
 import { UserSignupData } from '@/types/auth';
 
 export default function CreateAccountPage() {
-  const [isOtpSent, setIsOtpSent] = useState(true);
-  const [isOtpVerified, setIsOtpVerified] = useState(true);
+  const [isOtpSent, setIsOtpSent] = useState(false);
+  const [isOtpVerified, setIsOtpVerified] = useState(false);
   const [userData, setUserData] = useState<UserSignupData>({
     firstName: '',
     lastName: '',
