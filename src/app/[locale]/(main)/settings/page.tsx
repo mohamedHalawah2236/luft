b140cb6cafd0fb08e531ba3ea8035e729553a250
mohamedHalawah2236 <mohamedmahmoudhalawa22@gmail.com>
2026-03-02 15:19:32 +0200

    translate settings page

--- src/app/[locale]/(main)/settings/page.tsx
+++ src/app/[locale]/(main)/settings/page.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
 import Link from 'next/link';
+import { getTranslations } from 'next-intl/server';
 
-import { CircleChevronLeftIcon } from 'lucide-react';
+import { CircleChevronLeftIcon, CircleChevronRightIcon } from 'lucide-react';
 
 import ProfileForm from './_components/ProfileForm';
 
-export default function SettingsPage() {
+export default async function SettingsPage({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const t = await getTranslations('settings');
+  const { locale } = await params;
+  const isRtl = locale === 'ar';
+
   return (
     <div className='flex w-full gap-8 max-md:flex-col max-md:gap-6 max-md:py-20 max-sm:pb-40 max-sm:pt-6 md:py-14'>
       {/* Section Title */}
@@ -15,14 +24,18 @@ export default function SettingsPage() {
           href='/'
           className='w-fit max-sm:hidden'
         >
-          <CircleChevronLeftIcon className='size-10 stroke-1 text-grayish-900' />
+          {isRtl ? (
+            <CircleChevronRightIcon className='size-10 stroke-1 text-grayish-900' />
+          ) : (
+            <CircleChevronLeftIcon className='size-10 stroke-1 text-grayish-900' />
+          )}
         </Link>
         <div className='flex flex-col gap-2'>
           <h6 className='text-2xl font-medium text-grayish-900 sm:text-[1.75rem] md:max-w-36 md:text-[2rem]'>
-            Account Settings
+            {t('title')}
           </h6>
           <p className='text-lg text-grayish-400 md:hidden'>
-            Setup and edit your account profile
+            {t('description')}
           </p>
         </div>
       </div>
@@ -30,7 +43,7 @@ export default function SettingsPage() {
       {/* Form */}
       <div className='flex flex-1 flex-col gap-12'>
         <p className='text-lg text-grayish-400 max-md:hidden'>
-          Setup and edit your account profile
+          {t('description')}
         </p>
 
         <ProfileForm />
