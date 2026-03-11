585025be1f1318f29b16acb2289b8ce4cd9ea989
mohamedHalawah2236 <mohamedmahmoudhalawa22@gmail.com>
2026-03-11 13:03:02 +0200

    prevent writing scripts on profile form

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -3,3 +3,4 @@ export const NO_SPECIAL_CHARS = /^[A-Za-z\u0600-\u06FF\s]+$/;
 export const NO_NUMBERS = /^[^0-9]*$/;
 export const NUMBERS_ONLY = /^\d+$/;
 export const EGYPTIAN_PHONE = /^01[0125][0-9]{8}$/;
+export const NO_SCRIPTS = /<[^>]*>/;
--- src/types/sharedSchemas.ts
+++ src/types/sharedSchemas.ts
@@ -3,6 +3,7 @@ import z from 'zod';
 import { TFunction } from '@/constants';
 import {
   NO_NUMBERS,
+  NO_SCRIPTS,
   NO_SPECIAL_CHARS,
   NOT_SPACES_ONLY,
 } from '@/constants/regex';
@@ -13,4 +14,5 @@ export const nameSchema = (tRoot: TFunction) =>
     .min(1, tRoot('common.validations.required'))
     .regex(NOT_SPACES_ONLY, tRoot('common.validations.required'))
     .regex(NO_SPECIAL_CHARS, tRoot('common.validations.lettersOnly'))
-    .regex(NO_NUMBERS, tRoot('common.validations.lettersOnly'));
+    .regex(NO_NUMBERS, tRoot('common.validations.lettersOnly'))
+    .regex(NO_SCRIPTS, tRoot('common.validations.lettersOnly'));
