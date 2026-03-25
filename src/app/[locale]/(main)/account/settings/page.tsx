3651324b4613e1bc02de28bd243c869f06611c56
mohamedHalawah2236 <mohamedmahmoudhalawa22@gmail.com>
2026-03-25 10:12:47 +0200

    fix settings on md

--- src/app/[locale]/(main)/account/settings/page.tsx
+++ src/app/[locale]/(main)/account/settings/page.tsx
@@ -20,7 +20,7 @@ export default async function SettingsPage({
   const accessToken = session?.accessToken;
 
   return (
-    <div className='container flex w-full gap-8 max-lg:flex-col max-md:gap-6 md:mb-44 md:mt-20 xl:mb-48 xl:ms-[4.5rem] xl:mt-28 xl:w-[60.375rem]'>
+    <div className='container mb-[9.875rem] mt-6 flex w-full gap-8 max-lg:flex-col max-md:gap-6 md:mb-44 md:mt-20 xl:mb-48 xl:ms-[4.5rem] xl:mt-28 xl:w-[60.375rem]'>
       {/* Section Title */}
       <div className='flex gap-4 max-lg:items-center lg:flex-col xl:-mt-16'>
         <Link
@@ -42,8 +42,6 @@ export default async function SettingsPage({
           </p>
         </div>
       </div>
-
-      {/* Continute MD work */}
 
       {/* Form */}
       <div className='flex flex-1 flex-col gap-12'>
