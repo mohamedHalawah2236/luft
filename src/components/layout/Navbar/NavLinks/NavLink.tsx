aa357ff89d25fc4f4fe48b2a4b2951e7b32b2b08
mohamedHalawah2236 <mohamedmahmoudhalawa22@gmail.com>
2026-03-30 16:26:07 +0200

    fix active home nav and footer link

--- src/components/layout/Navbar/NavLinks/NavLink.tsx
+++ src/components/layout/Navbar/NavLinks/NavLink.tsx
@@ -6,15 +6,16 @@ import { useParams } from 'next/navigation';
 import { NavLinkProps } from '@/types/components';
 import { PageTypeEnum } from '@/types/page';
 
+import { usePathname } from '@/i18n/routing';
 import { cn } from '@/lib/utils';
 
 export default function NavLink({ title, id, pageType }: NavLinkProps) {
   const params = useParams();
+  const pathname = usePathname();
 
   const currentId = params?.id as string;
-  const isActive = currentId
-    ? currentId === id
-    : pageType === PageTypeEnum.Home;
+  const isHomePage = pageType === PageTypeEnum.Home && pathname === '/';
+  const isActive = currentId ? currentId === id : isHomePage;
 
   const href = pageType === PageTypeEnum.Home ? '/' : `/${id}`;
 
