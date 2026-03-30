c2da0434a0d1f89e9be5e7e4db15857db520281c
mohamedHalawah2236 <mohamedmahmoudhalawa22@gmail.com>
2026-03-30 17:07:15 +0200

    remove extra spacings in home page

--- src/components/pages/Home/HomePage.tsx
+++ src/components/pages/Home/HomePage.tsx
@@ -13,10 +13,8 @@ export default function HomePage({
 }: HomePageSections) {
   return (
     <div className='w-full bg-grayish-30'>
-      <div className='container flex w-full flex-col'>
-        <div className='mb-20 mt-6 md:mb-[7.5rem] md:mt-10 xl:my-[8.5rem]'>
-          <HeroSectionPreview {...hero} />
-        </div>
+      <div className='container mb-20 mt-6 flex w-full flex-col md:mb-[7.5rem] md:mt-10 xl:my-[8.5rem]'>
+        <HeroSectionPreview {...hero} />
       </div>
       <div className='my-20 bg-grayish-50 py-6 md:my-[7.5rem] md:py-12 xl:my-[8.75rem] xl:py-20'>
         <div className='container'>
