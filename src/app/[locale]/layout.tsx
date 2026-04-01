6d5b82ab1ffe8380aa2abf07f82c442d3e45891a
mohamedHalawah2236 <mohamedmahmoudhalawa22@gmail.com>
2026-04-01 10:03:41 +0200

    fix eslint errors

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,8 +1,8 @@
 import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
 import { getServerSession } from 'next-auth';
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
-import { notFound } from 'next/navigation';
 
 import ConnectionListener from '@/components/ConnectionListener';
 import Providers from '@/components/layout/Providers';
