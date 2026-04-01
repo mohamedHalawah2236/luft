d9d60b60840aa6d6daaf267d62b0ed3ea55d51b6
mohamedHalawah2236 <mohamedmahmoudhalawa22@gmail.com>
2026-04-01 11:35:14 +0200

    fix eslint errors

--- src/components/sections/TestimonialsSection/TestimonialsSection.tsx
+++ src/components/sections/TestimonialsSection/TestimonialsSection.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useTranslations } from 'next-intl';
 import Image from 'next/image';
+import { useTranslations } from 'next-intl';
 
 import { Star } from 'lucide-react';
 
