19c1cb265468bd8031b1b5c2a4ef4f117df2c9f7
Mohamed Halawa <mohamedmahmoudhalawa22@gmail.com>
2026-04-05 14:26:14 +0200

    fix footer overflow v2

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -23,15 +23,15 @@ export default async function Footer({
   const t = await getTranslations();
 
   return (
-    <div className='flex min-h-fit flex-col gap-6 overflow-x-hidden bg-grayish-900 px-6 py-10 text-grayish-100 md:gap-16 lg:py-11 xl:gap-[7.5rem] xl:px-[4.5rem]'>
-      <div className='flex flex-wrap justify-between gap-4 max-sm:flex-col max-sm:gap-6'>
+    <div className='flex flex-col gap-6 bg-grayish-900 px-6 py-10 text-grayish-100 md:gap-16 lg:py-11 xl:gap-[7.5rem] xl:px-[4.5rem]'>
+      <div className='flex justify-between gap-4 max-md:flex-col max-md:gap-6'>
         {/* First Column */}
         <div className='flex flex-col gap-6'>
           <Logo className='h-[3.75rem] w-[8.75rem] fill-white' />
-          <p className='text-grayish-100 sm:text-lg md:max-w-[21rem]'>
+          <p className='text-grayish-100 md:max-w-[21rem] md:text-lg'>
             {t('footer.tagline')}
           </p>
-          <div className='flex flex-col gap-6 max-sm:hidden'>
+          <div className='flex flex-col gap-6 max-md:hidden'>
             <RatedByGuests
               textClassName='text-lg'
               color='var(--ps-neutral-100)'
@@ -47,33 +47,35 @@ export default async function Footer({
           </div>
         </div>
 
-        {/* Second Column */}
-        <div className='flex flex-col gap-2'>
-          <span className='text-sm leading-6'>{t('common.labels.menu')}</span>
-          <div className='flex flex-col gap-4'>
-            {navLinks.map(({ title, id, pageType }) => (
-              <FooterLink
-                key={`footerLink-${id}`}
-                {...{ id, title, href: `/${id}`, pageType }}
+        <div className='flex gap-4 max-xl:max-w-[311px] max-xl:flex-1 max-md:flex-col xl:w-[40rem] xl:gap-10'>
+          {/* Second Column */}
+          <div className='flex flex-col gap-2 xl:flex-1'>
+            <span className='text-sm leading-6'>{t('common.labels.menu')}</span>
+            <div className='flex flex-col gap-4'>
+              {navLinks.map(({ title, id, pageType }) => (
+                <FooterLink
+                  key={`footerLink-${id}`}
+                  {...{ id, title, href: `/${id}`, pageType }}
+                />
+              ))}
+            </div>
+          </div>
+
+          {/* Third Column */}
+          <div className='flex flex-1 flex-col gap-4 overflow-hidden max-md:mt-6 xl:flex-1'>
+            {contactItems.map(({ description, id, title }) => (
+              <ContactItem
+                key={id}
+                label={title}
+                value={description}
               />
             ))}
           </div>
         </div>
 
-        {/* Third Column */}
-        <div className='flex flex-col gap-4 max-sm:mt-6'>
-          {contactItems.map(({ description, id, title }) => (
-            <ContactItem
-              key={id}
-              label={title}
-              value={description}
-            />
-          ))}
-        </div>
-
-        <div className='mt-6 flex flex-col gap-6 sm:hidden'>
+        <div className='mt-6 flex flex-col gap-6 md:hidden'>
           <RatedByGuests
-            textClassName='sm:text-lg'
+            textClassName='md:text-lg'
             color='var(--ps-neutral-100)'
           />
           <div className='flex items-center gap-5'>
