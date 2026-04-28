14d3651b2a05bedd600e432cc354e374d3ff6a07
mohamedHalawah2236 <mohamedmahmoudhalawa22@gmail.com>
2026-04-28 17:32:05 +0300

    enhance layout to scroll to the top when navigating between the app including the loader page

--- src/app/[locale]/(main)/loading.tsx
+++ src/app/[locale]/(main)/loading.tsx
@@ -1,8 +1,10 @@
-'use client';
-
 import Loader from '@/components/shared/Loader';
 
 function Loading() {
-  return <Loader />;
+  return (
+    <div className='flex h-screen min-h-dvh items-center justify-center'>
+      <Loader />
+    </div>
+  );
 }
 export default Loading;
