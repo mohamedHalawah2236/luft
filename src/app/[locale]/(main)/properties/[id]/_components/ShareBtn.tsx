35041424a9cee18eb026f7dd8ec2bbc7acd6766f
mohamedHalawah2236 <mohamedmahmoudhalawa22@gmail.com>
2026-04-29 14:56:16 +0300

    apply Share Property functionality

--- src/app/[locale]/(main)/properties/[id]/_components/ShareBtn.tsx
+++ src/app/[locale]/(main)/properties/[id]/_components/ShareBtn.tsx
@@ -1,14 +1,34 @@
+'use client';
 import { useTranslations } from 'next-intl';
 
 import { Upload } from 'lucide-react';
+import { toast } from 'sonner';
 
 export default function ShareBtn() {
   const t = useTranslations('pages.propertyDetails.actions');
-
+  console.log(window.location.href);
   return (
-    <div className='flex items-center gap-2'>
+    <button
+      onClick={() => {
+        const url = window.location.href;
+
+        if (navigator.share) {
+          navigator
+            .share({
+              title: t('shareProperty'),
+              text: t('shareProperty'),
+              url,
+            })
+            .catch((err) => console.log(err));
+        } else {
+          navigator.clipboard.writeText(url);
+          toast.info(t('linkCopied'));
+        }
+      }}
+      className='flex items-center gap-2'
+    >
       <Upload className='size-6 text-grayish-400' />
       <span className='text-grayish-900 max-xl:hidden'>{t('share')}</span>
-    </div>
+    </button>
   );
 }
