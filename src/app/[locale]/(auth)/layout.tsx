2dd32f827596bdbf288c87414d9f132387b9fe7e
mohamedHalawah2236 <mohamedmahmoudhalawa22@gmail.com>
2026-04-29 15:25:20 +0300

    auth layout enhancements

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -10,7 +10,7 @@ import QueryClientProvider from '@/providers/QueryClientProvider';
 export default function layout({ children }: { children: ReactNode }) {
   return (
     <div className='container flex h-screen items-center justify-between gap-8 bg-grayish-30 lg:p-2'>
-      <div className='flex flex-1 flex-col gap-8 max-lg:p-4 max-lg:px-10 max-md:px-3.5 lg:gap-12 lg:pe-1 lg:ps-6'>
+      <div className='flex max-h-full flex-1 flex-col gap-8 overflow-auto max-lg:p-4 max-lg:px-10 max-md:px-3.5 lg:gap-12 lg:pe-1 lg:ps-6'>
         <Link
           href='/'
           className='mt-2 size-fit max-lg:mx-auto lg:mt-11'
